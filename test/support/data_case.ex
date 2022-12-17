6593b3e0d5bd9eb6f5f210e8d18d2e15b1961094
Alessio Biancalana <dottorblaster@gmail.com>
2022-12-17 12:09:24 +0100

    Alias SQL sanbox

--- test/support/data_case.ex
+++ test/support/data_case.ex
@@ -16,6 +16,8 @@ defmodule Talisman.DataCase do
 
   use ExUnit.CaseTemplate
 
+  alias Ecto.Adapters.SQL.Sandbox
+
   using do
     quote do
       alias Talisman.Repo
@@ -36,8 +38,8 @@ defmodule Talisman.DataCase do
   Sets up the sandbox based on the test tags.
   """
   def setup_sandbox(tags) do
-    pid = Ecto.Adapters.SQL.Sandbox.start_owner!(Talisman.Repo, shared: not tags[:async])
-    on_exit(fn -> Ecto.Adapters.SQL.Sandbox.stop_owner(pid) end)
+    pid = Sandbox.start_owner!(Talisman.Repo, shared: not tags[:async])
+    on_exit(fn -> Sandbox.stop_owner(pid) end)
   end
 
   @doc """
