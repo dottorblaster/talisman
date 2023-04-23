7664951ed5cf2ffc5a6f94ff6c7cdd3a66ec6bc3
Alessio Biancalana <dottorblaster@gmail.com>
2023-04-23 22:42:50 +0200

    Add tailwind installation

--- config/dev.exs
+++ config/dev.exs
@@ -26,7 +26,8 @@ config :talisman, TalismanWeb.Endpoint,
   secret_key_base: "bqjp5mOfWv6u5Lclah0GOBp2ugfSS0gf2lOv9g/kmnoa0eXadAzd225gD4WVWcwr",
   watchers: [
     # Start the esbuild watcher by calling Esbuild.install_and_run(:default, args)
-    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]}
+    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]},
+    tailwind: {Tailwind, :install_and_run, [:default, ~w(--watch)]}
   ]
 
 # ## SSL Support
