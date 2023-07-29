4da547ee93c94c4e48750f468c6ba90dcfc95211
Alessio Biancalana <dottorblaster@gmail.com>
2023-07-29 17:56:45 +0200

    Add Earmark as a dep

--- mix.exs
+++ mix.exs
@@ -60,7 +60,8 @@ defmodule Talisman.MixProject do
       {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
       {:faker, "~> 0.17", only: [:dev, :test]},
       {:ex_machina, "~> 2.7.0", only: :test},
-      {:tailwind, "~> 0.2.1", runtime: Mix.env() == :dev}
+      {:tailwind, "~> 0.2.1", runtime: Mix.env() == :dev},
+      {:earmark, "~> 1.4"}
     ]
   end
 
