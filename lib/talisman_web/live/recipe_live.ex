a66911b133af0eff85b3afcbdc8aded848e9688d
Alessio Biancalana <dottorblaster@gmail.com>
2023-07-29 17:56:56 +0200

    Enhance UI styling, a lil bit

--- lib/talisman_web/live/recipe_live.ex
+++ lib/talisman_web/live/recipe_live.ex
@@ -1,6 +1,7 @@
 defmodule TalismanWeb.RecipeLive do
   use TalismanWeb, :live_view
 
+  alias Earmark.Options
   alias Talisman.Cookbooks
 
   on_mount TalismanWeb.UserLiveAuth
@@ -15,10 +16,14 @@ defmodule TalismanWeb.RecipeLive do
   def render(assigns) do
     ~H"""
     <span>
-      <h1><%= @recipe.name %></h1>
-      <div><%= @recipe.ingredients %></div>
-      <div><%= @recipe.recipe %></div>
+      <h1 class="mx-10 my-5 font-bold text-3xl"><%= @recipe.name %></h1>
+      <div class="mx-10 mb-10"><%= @recipe.ingredients %></div>
+      <div class="mx-10"><%= as_html(@recipe.recipe) %></div>
     </span>
     """
+  end
+
+  defp as_html(markdown) do
+    markdown |> Earmark.as_html!() |> raw
   end
 end
