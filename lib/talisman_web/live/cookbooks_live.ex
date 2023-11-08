b64b7eb709cb8af46b4a7adaed37a7c89b8d5f6e
Alessio Biancalana <alessio@dottorblaster.it>
2023-11-08 18:47:00 +0100

    Better cookbook layout and better Navmenu

--- lib/talisman_web/live/cookbooks_live.ex
+++ lib/talisman_web/live/cookbooks_live.ex
@@ -14,13 +14,30 @@ defmodule TalismanWeb.CookbooksLive do
 
   def render(assigns) do
     ~H"""
-    <span>
+    <div class="mt-6 px-24">
       <%= for cookbook <- assigns.cookbooks do %>
         <div>
-          <a href={"/cookbook/#{cookbook.uuid}"}><%= cookbook.name %></a>
+          <a href={"/cookbook/#{cookbook.uuid}"}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              class="w-6 h-6 inline"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M16.5 3.75V16.5L12 14.25 7.5 16.5V3.75m9 0H18A2.25 2.25 0 0120.25 6v12A2.25 2.25 0 0118 20.25H6A2.25 2.25 0 013.75 18V6A2.25 2.25 0 016 3.75h1.5m9 0h-9"
+              />
+            </svg>
+
+            <%= cookbook.name %>
+          </a>
         </div>
       <% end %>
-    </span>
+    </div>
     """
   end
 end
