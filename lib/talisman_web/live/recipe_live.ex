09b1cd5ee7154de07281ed146d7295d6c8c56064
Alessio Biancalana <alessio@dottorblaster.it>
2023-11-12 13:44:45 +0100

    Add edit button

--- lib/talisman_web/live/recipe_live.ex
+++ lib/talisman_web/live/recipe_live.ex
@@ -3,24 +3,42 @@ defmodule TalismanWeb.RecipeLive do
 
   alias Talisman.Cookbooks
 
+  import TalismanWeb.Components.Button
+
   on_mount TalismanWeb.UserLiveAuth
 
   def mount(params, _session, socket) do
     recipe_id = Map.get(params, "recipe_id")
     recipe = Cookbooks.get_recipe!(recipe_id)
 
-    {:ok, assign(socket, recipe: recipe)}
+    {:ok, assign(socket, recipe: recipe, recipe_id: recipe_id)}
   end
 
   def render(assigns) do
     ~H"""
-    <span>
-      <h1 class="mx-10 my-5 font-bold text-3xl"><%= @recipe.name %></h1>
-      <div class="mx-10 mb-10"><%= @recipe.ingredients %></div>
-      <div class="mx-10"><%= as_html(@recipe.recipe) %></div>
-    </span>
+    <div>
+      <div>
+        <h1 class="mx-10 my-5 font-bold text-3xl"><%= @recipe.name %></h1>
+        <div class="mx-10 mb-10"><%= @recipe.ingredients %></div>
+        <div class="mx-10"><%= as_html(@recipe.recipe) %></div>
+      </div>
+      <div class="mx-10 mt-10">
+        <.button phx-click="recipe_edit">Edit</.button>
+      </div>
+    </div>
     """
   end
+
+  def handle_event(
+        "recipe_edit",
+        _payload,
+        %{
+          assigns: %{
+            recipe_id: recipe_id
+          }
+        } = socket
+      ),
+      do: {:noreply, push_navigate(socket, to: ~p"/recipe/#{recipe_id}/edit")}
 
   defp as_html(markdown) do
     markdown |> Earmark.as_html!() |> raw
