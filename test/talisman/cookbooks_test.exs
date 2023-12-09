99b6b15ad4a0cb7c56e8212f6c676ac5ee00ebe4
Alessio Biancalana <alessio@dottorblaster.it>
2023-12-09 16:51:03 +0100

    Test reicpe deletion

--- test/talisman/cookbooks_test.exs
+++ test/talisman/cookbooks_test.exs
@@ -187,5 +187,46 @@ defmodule Talisman.CookbooksTest do
                ]
              } = Cookbooks.get_cookbook(cookbook_uuid)
     end
+
+    test "create a cookbook, append a recipe to it, with an empty category, then delete it" do
+      %{author_uuid: author_uuid} =
+        new_cookbook = %{author_uuid: Faker.UUID.v4(), name: Faker.Pokemon.name(), recipes: []}
+
+      assert :ok = Cookbooks.create_cookbook(new_cookbook)
+
+      [%Cookbook{uuid: cookbook_uuid}] = Cookbooks.get_cookbooks_by_author_uuid(author_uuid)
+
+      %{name: recipe_name, recipe: recipe_body, ingredients: recipe_ingredients} =
+        new_recipe = %{
+          cookbook_uuid: cookbook_uuid,
+          author_uuid: author_uuid,
+          name: Faker.Food.dish(),
+          recipe: Faker.Lorem.paragraph(),
+          ingredients: [Faker.Food.ingredient(), Faker.Food.ingredient()]
+        }
+
+      assert :ok = Cookbooks.add_recipe(new_recipe)
+
+      assert %Cookbook{
+               uuid: ^cookbook_uuid,
+               recipes: [
+                 %Recipe{
+                   uuid: recipe_uuid,
+                   name: ^recipe_name,
+                   recipe: ^recipe_body,
+                   ingredients: ^recipe_ingredients
+                 }
+               ]
+             } = Cookbooks.get_cookbook(cookbook_uuid)
+
+      Cookbooks.delete_recipe(author_uuid, %{
+        cookbook_uuid: cookbook_uuid,
+        recipe_uuid: recipe_uuid
+      })
+
+      assert_raise(Ecto.NoResultsError, fn ->
+        Cookbooks.get_recipe!(recipe_uuid)
+      end)
+    end
   end
 end
