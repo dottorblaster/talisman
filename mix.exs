01aed6d89d43b8d4184ef58047a6e7debee87035
Alessio Biancalana <alessio@dottorblaster.it>
2025-08-10 17:08:03 +0200

    deps: bump to pheonix 1.8

--- mix.exs
+++ mix.exs
@@ -34,7 +34,7 @@ defmodule Talisman.MixProject do
   defp deps do
     [
       {:bcrypt_elixir, "~> 3.0"},
-      {:phoenix, "~> 1.7.3"},
+      {:phoenix, "~> 1.8.0"},
       {:phoenix_ecto, "~> 4.4"},
       {:ecto_sql, "~> 3.6"},
       {:postgrex, ">= 0.0.0"},
